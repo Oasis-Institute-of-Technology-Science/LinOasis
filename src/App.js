9cd1f3c7242d58d93753783c49e5941fa98b0eaa
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-03-20 08:58:48 +0800

    Task 1: Project Setup & Configuration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/main.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello LinOasis</h1>
+      <p>Interactive Linear Algebra Visualization Platform</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]',
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'videos/[name].[hash][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+};
